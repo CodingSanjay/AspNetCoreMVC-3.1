39fde7e75b25d9472edc39af4d9e8f696492b77f
SanajayDev <sanjay.kumr1@gmail.com>
2023-01-18 13:45:12 +0530

    Using Avatar for static files

--- BookStore/BookStore/Startup.cs
+++ BookStore/BookStore/Startup.cs
@@ -3,9 +3,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -32,6 +34,8 @@ namespace BookStore
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                var currentcontent = env.EnvironmentName;
+
             }
             else
             {
@@ -41,8 +45,15 @@ namespace BookStore
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            //------------This is Method is using for get/read the static files from anothor content Root Folder
+            //app.UseStaticFiles(new StaticFileOptions()
+            //{
+            //    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "MyFiles")),
+            //    RequestPath = "/MyFiles"
+            //}) ;
 
             app.UseRouting();
+
 
             app.UseAuthorization();
 
